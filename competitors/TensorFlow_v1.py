ed2c3aaf057ed1732d552f5296113d2003b65ad2
JontePonte <john_nordstrand@hotmail.com>
2019-09-27 15:07:00 +0200

    More working but not working

--- competitors/TensorFlow_v1.py
+++ competitors/TensorFlow_v1.py
@@ -3,6 +3,7 @@ from base_competitor import Competitor
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
+import itertools
 import random
 
 #TensorFlow_v1
@@ -12,6 +13,8 @@ class TF_v1(Competitor):
         self.name = "TF v1"
         self.epochs_num = 5
 
+        self.correct_choice = [0]*nr
+
     def choose(self):
 
         if self.round == 0:
@@ -20,16 +23,22 @@ class TF_v1(Competitor):
             """ Import data in a training set and a test set """
             other = self.otherchoices[0:self.round]
             my = self.mychoices[0:self.round]
-            input_data = other + my
-            input_data = input_data / 3     # Data is flat and minimized
-            correct_choice = self.get_right_choice()
+            correct_choice = self.correct_choice[0:self.round]
+            input_data = other + my + correct_choice
+
+            ### Fix correct data vector [0,0,1]
+
+
+            print(input_data)
+            print(correct_choice)
+
 
             """ Create structure for the Neural Network """
 
             model = keras.Sequential([
-                keras.layers.Flatten(input_shape=(self.round, 1)),
+                keras.layers.Input(range(input_data)),      ### Figure this one out...
                 keras.layers.Dense(32, activation="relu"),
-                keras.layers.Dense(3, activation="softmax")
+                keras.layers.Dense(self.round, activation="softmax")
                 ])
             # Set optimizer, loss function and correction metrics
             model.compile(optimmizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
@@ -40,17 +49,16 @@ class TF_v1(Competitor):
             """ Predict data with the NN model """
             prediction = model.predict(test_images)
 
+        self.correct_choice[self.round] = self.get_right_choice()
+
     # Method that creates a vector with all the right choices
     def get_right_choice(self):
-        output = [0] * self.round
-        for i in range(self.round):
-            if self.otherchoices[i] == 1:
-                right_choice = 2
-            if self.otherchoices[i] == 2:
-                right_choice = 3
-            if self.otherchoices[i] == 3:
-                right_choice = 1
-            output[i] = right_choice
-        return output
-
+        right_choice = 1
+        if self.otherchoices[self.round] == 1:
+            right_choice = 2
+        elif self.otherchoices[self.round] == 2:
+            right_choice = 3
+        elif self.otherchoices[self.round] == 3:
+            right_choice = 1
+        return right_choice
 
